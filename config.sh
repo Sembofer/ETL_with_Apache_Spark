120259765f9fcd5a900495595d1295cc4f271f53
Sembofer <fernando.valencia.hdez@gmail.com>
2022-07-17 19:10:19 -0500

    config

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 ## Docker
-docker run -e POSTGRES_HOST_AUTH_METHOD=md5 -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 --name prueba -d postgres
+docker run -e POSTGRES_HOST_AUTH_METHOD=md5 -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 --name mypostgres -d postgres
 docker cp script.sql mypostgres:.
 docker exec -i -u postgres mypostgres bash -c "echo 'password_encryption=md5' >> /var/lib/postgresql/data/postgresql.conf"
 docker restart mypostgres
